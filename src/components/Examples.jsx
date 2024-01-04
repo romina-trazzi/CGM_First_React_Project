bcdebcf02e678abc11990be4304dc24a2fa4ea96
Romina Trazzi <romina.trazzi@posta.it>
2024-01-04 16:50:46 +0100

    Refactoring (04/01/24)

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,5 +1,6 @@
-import TabButton from './TabButton';
 import Section from './Section';
+import Tabs from './Tabs';
+import TabButton from './TabButton';
 import { useState } from 'react';
 import { EXAMPLES } from '../data.js';
 
@@ -12,52 +13,54 @@ function Examples() {
   }
   return (
     <Section id='examples' title='Examples'>
-      {/* Children in questo caso è il nome della Tab, e sta in mezzo 
-          al tag del componente (vedi il componente child) */}
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === 'components'}
-          onClick={() => handleClick('components')}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'jsx'}
-          onClick={() => handleClick('jsx')}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'props'}
-          onClick={() => handleClick('props')}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'state'}
-          onClick={() => handleClick('state')}
-        >
-          State
-        </TabButton>
-      </menu>
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onClick={() => handleClick('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onClick={() => handleClick('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onClick={() => handleClick('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onClick={() => handleClick('state')}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {/*  ALTERNATIVA: Se selectedTopic non esiste (alias è undefined):
+      {!selectedTopic && <p>Please select a topic </p> } */}
+
+        {selectedTopic ? (
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        ) : null}
+
+        {/*  ALTERNATIVA: Se selectedTopic esiste (alias è popolato):
+      {selectedTopic && ( <div><h3> ecc ecc </h3></div> ) ecc. } */}
+      </Tabs>
 
       {selectedTopic === undefined ? <p> Please select a topic. </p> : null}
-
-      {/*  ALTERNATIVA: Se selectedTopic non esiste (alias è undefined):
-    {!selectedTopic && <p>Please select a topic </p> } */}
-
-      {selectedTopic ? (
-        <div id='tab-content'>
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code}</code>
-          </pre>
-        </div>
-      ) : null}
-
-      {/*  ALTERNATIVA: Se selectedTopic esiste (alias è popolato):
-        {selectedTopic && ( <div><h3> ecc ecc </h3></div> ) ecc. } */}
     </Section>
   );
 }
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,10 +1,15 @@
-function Tabs() {
+function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
   return (
     <>
-      <menu></menu>
-      CONTENT
+      <ButtonsContainer>{buttons}</ButtonsContainer>
+      {children}
     </>
   );
 }
 
 export default Tabs;
+
+/* la prop ButtonContainer viene utilizzata come componente custom.
+Dichiarabile nel padre Examples viene riutilizzata qua. 
+L'uguale dopo la props è il valore di default ed è il motivo
+per cui non compare nel componente Examples in questo caso */
